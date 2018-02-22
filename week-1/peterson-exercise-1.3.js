55cc703437165111703be5ad41550d4d33e63efe
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-22 10:50:29 -0500

    added header and code contribution

---
+++ week-1/peterson-exercise-1.3.js
@@ -0,0 +1,10 @@
+var header = require('../header.js'); 
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   22 January 2018
+; Modified By: Heather Peterson
+; Description: Exercise 1.3 - Modules
+;===========================================
+*/ 
