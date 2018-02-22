5d778ecc4361f1a744bde4792d30e45fadcac7e8
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-22 10:49:45 -0500

    changed exercise number

---
+++ header.js
@@ -0,0 +1,23 @@
+var myFirstName = "Heather";
+var myLastName = "Peterson";
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+
+if (dd<10) {
+    dd = '0' + dd
+  }
+  
+  if (mm<10) {
+    mm = '0' + mm
+  }
+  
+  today = mm + '-' + dd + '-' + yyyy
+  var assignmentNum = "Exercise 1.3";
+  
+  var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+      + assignmentNum + "\nDate: " + today;
+  
+  console.log(programHeader);
+  console.log("\n");
