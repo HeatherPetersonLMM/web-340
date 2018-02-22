2cb4ed8fd686da397b6069c3836edf81881d8d66
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-22 11:00:49 -0500

    finalized code with code comments

--- week-1/peterson-exercise-1.3.js
+++ week-1/peterson-exercise-1.3.js
@@ -8,3 +8,21 @@ var header = require('../header.js');
 ; Description: Exercise 1.3 - Modules
 ;===========================================
 */ 
+
+var url = require('url');  //require statement for URL library
+var parsedURL = url.parse('https://github.com/HeatherPetersonLMM?name=Heather'); //variable to hold string URL value
+
+console.log(parsedURL.protocol); //parsed function used to output parts of string URL
+console.log(parsedURL.host);
+console.log(parsedURL.query);
+
+
+// Output results
+
+//Heather Peterson
+//Exercise 1.3
+//Date: 02-22-2018
+
+//https:
+//github.com
+//name=Heather
