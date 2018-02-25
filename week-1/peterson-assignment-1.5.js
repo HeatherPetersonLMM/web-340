d1cb0e82dc4f06d4446ad17c634280d70f279d17
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-25 15:49:39 -0500

    added code comments

--- week-1/peterson-assignment-1.5.js
+++ week-1/peterson-assignment-1.5.js
@@ -5,19 +5,19 @@ var header = require('../header.js');
 ; Author: Professor Krasso 
 ; Date:   February 25 2018
 ; Modified By: Heather Peterson
-; Description: Assignment 1.5 - Hello World
+; Description: Assignment 1.5 - Hello World - This is really neat!
 ;===========================================
 */ 
 
-var http = require("http");
+var http = require("http");  //creating statement for http library
 
-function processRequest(req, res) {
+function processRequest(req, res) {  //function 'processRequest' that outputs var body text value
 
-var body = "This is really neat!";
+var body = "This is really neat!";  // variable body output displayed in localhost:8080 of web browser
 
-    var contentLength = body.length;
+    var contentLength = body.length;  // variable to display body and length properties
 
-    res.writeHead(200, {
+    res.writeHead(200, {  // writes a response to client and adds a HTTP header
 
         'Content-Length': contentLength,
 
@@ -25,10 +25,10 @@ var body = "This is really neat!";
 
     });
 
-    res.end(body);
+    res.end(body);  //end of response
 
 }
 
-var s = http.createServer(processRequest);
+var s = http.createServer(processRequest); // creates server port for the processRequest
 
-s.listen(8080);
+s.listen(8080);  // server object listens on port 8080
