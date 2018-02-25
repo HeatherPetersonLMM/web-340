02fabab9fbf479dec350a73e9ffa76092de91148
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-25 15:18:29 -0500

    changed to assignment 1.5

--- header.js
+++ header.js
@@ -14,7 +14,7 @@ if (dd<10) {
   }
   
   today = mm + '-' + dd + '-' + yyyy
-  var assignmentNum = "Exercise 1.3";
+  var assignmentNum = "Assignment 1.5";
   
   var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
       + assignmentNum + "\nDate: " + today;
