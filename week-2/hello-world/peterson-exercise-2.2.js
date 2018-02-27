354feff76aed2f59e0e3aac6284e5277be1656b2
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-02-27 16:14:02 -0500

    added code attribution and code comments

--- week-2/hello-world/peterson-exercise-2.2.js
+++ week-2/hello-world/peterson-exercise-2.2.js
@@ -1,19 +1,26 @@
-var express = require('express');
-var http = require('http');
+/*
+============================================
+; Title:  Exercise 2.2
+; Author: Professor Krasso 
+; Date:   February 27 2018
+; Modified By: Heather Peterson
+; Description: Hello World with Express
+;===========================================
+*/ 
 
+var express = require('express');  //requires the Express module
+var http = require('http');  //requires the HTTP library to start the server
 
-var app = express();
+var app = express();  //calls the express function to start a new Express application
 
-
-app.use(function(req, res)
+app.use(function(req, res)  //routing function used to request and return a URL
 {
-    console.log('In comes a request to: %s', req.url);
+    console.log('In comes a request to: %s', req.url);  //logs message to console
 
-    res.end('Hello World\n');
+    res.end('Hello World\n');  //returns value of accessed URL
 })
 
-
-http.createServer(app).listen(8080, function() 
+http.createServer(app).listen(8080, function()  //creates a new server that listens on port 8080
 {
-    console.log('Application started on port %s', 8080);
+    console.log('Application started on port %s', 8080);  //logs output message of accessed port
 });
