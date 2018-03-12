528017373c763f962a4ad352dbddc28ad843c3d3
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-03-11 20:51:37 -0400

    added code attribution and comments

--- week-3/putting-it-all-together/peterson-assignment-3.4.js
+++ week-3/putting-it-all-together/peterson-assignment-3.4.js
@@ -1,40 +1,50 @@
-var express = require("express");
-var http = require("http");
-var path = require("path");
-var logger = require("morgan");
+/*
+============================================
+; Title:  Assignment 3.4
+; Author: Professor Krasso 
+; Date:   11 March 2018
+; Modified By: Heather Peterson
+; Description: Logging, Advanced Routing - Putting it all together
+;===========================================
+*/ 
 
-var app = express();
+var express = require("express");  // assigning express require statement to express variable
+var http = require("http");  // assigning http require statement to http variable
+var path = require("path");  // assigning path require statement to path variable
+var logger = require("morgan");  // assigning morgan require statement to logger variable
 
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
+var app = express();  // assigning express statement to app variable
 
-app.use(logger("short"));
+app.set("views", path.resolve(__dirname, "views"));  // Tell Express the views are in the 'views' directory
+app.set("view engine", "ejs");  // Tell Express to use the EJS view engine
 
-app.get("/", function(request, response) {
-    response.render("index", {
-        message: "home page"
+app.use(logger("short"));  // Tell Express to add the logger
+
+app.get("/", function(request, response) {  // HTTP with get request with '/' route function that accepts a request and has a response
+    response.render("index", {  //output to the indexed ejs page
+        message: "home page"  // output message
     });
 });
 
-app.get("/about", function(request, response) {
-    response.render("about", {
-        message: "about page"
+app.get("/about", function(request, response) {  // HTTP with get request with '/about' route function that accepts a request and has a response
+    response.render("about", {  //output to the indexed ejs page
+        message: "about page"  // output message
     });
 });
 
-app.get("/contact", function(request, response) {
-    response.render("contact", {
-        message: "contact page"
+app.get("/contact", function(request, response) {  // HTTP with get request with '/contact' route function that accepts a request and has a response
+    response.render("contact", {  //output to the indexed ejs page
+        message: "contact page"  // output message
     })
 });
 
-app.get("/products", function(request, response) {
-   response.render("products", {
-       message: "products page"
+app.get("/products", function(request, response) {  // HTTP with get request with '/products' route function that accepts a request and has a response
+   response.render("products", {  //output to the indexed ejs page
+       message: "products page"  // output message
    });
 });
 
 
-http.createServer(app).listen(8080, function() {
-   console.log("Application started on port 8080.");
+http.createServer(app).listen(8080, function() {  // creates the server to listen on port 8080
+   console.log("Application started on port 8080.");  // logs output message on port 8080
 });
