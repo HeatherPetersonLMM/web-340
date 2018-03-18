b34ea3a677092ed6716e07d7dddbee8a9581ddac
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-03-18 13:13:09 -0400

    added exercise 4.3 finalized code with attribution and comments

--- week-4/status-codes/peterson-exercise-4.3.js
+++ week-4/status-codes/peterson-exercise-4.3.js
@@ -9,32 +9,37 @@
 */ 
 
 
-var express = require("express");
-var http = require("http");
+var express = require("express");  // assigning express require statement to express variable
+var http = require("http");  // assigning http require statement to http variable
+var logger = require("morgan");  //uses morgan to log output in console window
 
-var app = express();
 
-app.get("/not-found", function(request, response) {
-    response.status(404);
-    response.json({
-        error: "Resource not found."
+var app = express(); // registers the application
+
+app.use(logger("dev"));  // specifies the logger using dev
+
+// requests using status codes
+app.get("/not-found", function(req, res) { // creates GET request for 404 response error
+    res.status(404);
+    res.json({  // response output of the json object
+        error: "Page Not Found."  // output response
     })
 });
 
-app.get("/ok", function(request, response) {
-    response.status(200);
-    response.json({
-        message: "Page loaded correctly."
+app.get("/ok", function(req, res) {  // creates GET request for 200 response error
+    res.status(200);
+    res.json({  // response output of the json object
+        error: "Page OK."  // output response
     })
 });
 
-app.get("/not-implemented", function(request, response) {
-    response.status(501);
-    response.json({
-        error: "Page not implemented."
+app.get("/not-implemented", function(req, res) {  // creates GET request for 501 response error
+    res.status(501);
+    res.json({  ////response output of the json object
+        error: "Page Not Executed."  // output response
     })
 });
 
-http.createServer(app).listen(8080, function() {
-   console.log("Application started on port 8080!");
+http.createServer(app).listen(7070, function() {  // creates the server to listen on port 7070
+   console.log("Application started on port 7070.");  // logs output message on port 7070
 });
