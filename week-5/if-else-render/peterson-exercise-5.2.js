8e4fe9860a0288ea033844c69308a568a83c2027
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-03-21 14:57:57 -0400

    added finalized coding for exercise 5.2

--- week-5/if-else-render/peterson-exercise-5.2.js
+++ week-5/if-else-render/peterson-exercise-5.2.js
@@ -1,26 +1,33 @@
+
+// requires
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 
-app = express();
+// app functions
+var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
-var f = [
-  "Apple",
-  "Blueberry",
-  "Orange",
-  "Strawberry"
+// local composer array
+var composers = [
+  "Bach",
+  "Mozart",
+  "Beethoven",
+  "Verdi"
 ];
 
-app.get("/", function(request, response) {
-    response.render("index", {
-        fruits: f
+// routes
+app.get("/", function(req, res) {
+    res.render("index", {
+        names: composers
     })
 });
 
+
+// create server
 http.createServer(app).listen(8080, function() {
     console.log("Application started on port 8080!");
 });
