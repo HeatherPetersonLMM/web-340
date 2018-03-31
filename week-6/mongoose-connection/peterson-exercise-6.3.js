83d25c13ecf3ec7414aebf207afb39780e287eca
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-03-30 20:04:34 -0400

    added finalized code with code attribution and comments

---
+++ week-6/mongoose-connection/peterson-exercise-6.3.js
@@ -0,0 +1,44 @@
+/*
+============================================
+; Title:  Exercise 6.3
+; Author: Professor Krasso 
+; Date:   30 March 2018
+; Modified By: Heather Peterson
+; Description: Mongoose
+;===========================================
+*/ 
+
+// require
+var express = require('express');
+var http = require('http');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+// specify mongoDB connection string
+var mongoDB = 'mongodb://hpeterson.lmm:Rum1chata@ds229549.mlab.com:29549/ems';
+mongoose.connect(mongoDB, { // create connection
+    useMongoClient: true
+});
+mongoose.Promise = global.Promise;  // add Promise
+var db = mongoose.connection;  // create database variable
+
+//error handling
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+db.once('open', function() {
+    console.log('Application connected to mLab.');
+});
+
+// app function and route
+var app = express();
+app.use(logger('dev'));
+
+
+// create server
+http.createServer(app).listen(5000, function (){
+    console.log('Application started and listening on port 5000.');
+});
+
+// Output result (in terminal window)
+//
+//Application started and listening on port 5000.
+//Application connected to mLab.
