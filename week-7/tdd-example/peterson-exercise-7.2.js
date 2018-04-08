49c14a6cc35c415b2dfb27f927eddde9cdad88d9
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-04-08 15:39:10 -0400

    added finalized code with comments and attribution

---
+++ week-7/tdd-example/peterson-exercise-7.2.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  Exercise 7.2
+; Author: Professor Krasso 
+; Date:   7 April 2018
+; Modified By: Heather Peterson
+; Description: TDD in Action
+;===========================================
+*/
+
+//requires
+var mocha = require("mocha");
+var chai = require("chai");
+var assert = require("assert");
+
+//testing
+describe("String#split", function(){
+    it("should return an array of fruits", function(){
+        assert(Array.isArray('Apple, Orange, Mango' .split(',')));
+    });
+});
